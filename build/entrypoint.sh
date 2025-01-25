849b745446df59ef4db58c14d861e648bbee2ed5
NOUSRADINE Cherif Hassan <cherif-hassan.nousradine@loria.fr>
2025-01-25 14:40:09 +0100

    improve code

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -13,7 +13,6 @@ fi
 
 sleep 5
 
-/usr/local/bin/gunicorn -w 4 -k uvicorn.workers.UvicornWorker -t 250 -b 0.0.0.0:5000 main:app --reload #$reload_opt 
+/usr/local/bin/gunicorn -w 4 -k uvicorn.workers.UvicornWorker -t 250 -b 0.0.0.0:8000 main:app --reload #$reload_opt 
 
-# /usr/local/bin/gunicorn -w $NB_WORKERS -k uvicorn.workers.UvicornWorker -t 120 -b 0.0.0.0:5000 main:app --reload #$reload_opt 
 
